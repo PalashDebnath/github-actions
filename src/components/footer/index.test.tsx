0c91a6a44261474e7fa0506eb07d68c4b067001e
Debnath <pnl04832@Emea.Royalahold.net>
2023-01-14 13:32:23 +0100

    Fixed test

--- src/components/footer/index.test.tsx
+++ src/components/footer/index.test.tsx
@@ -5,7 +5,7 @@ import FooterComponent from './index';
 describe('Footer component', () => {
     test('1. Should render "Privacy" text', () => {
         render(<FooterComponent />);
-        const text = screen.getByText(/hello/);
+        const text = screen.getByText(/Privacy/);
         expect(text).toBeDefined();
     });
 });
